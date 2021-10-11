334d5ae7c566aae7a70c2e4ad70d727477605abd
shubham sharma <shubhamsharma585@gmail.com>
2021-10-11 12:51:20 +0530

    app initiated

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './Components/Home';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Home() {
+    return (
+        <div>
+            <h1>Chat</h1>
+        </div>
+    )
+}
+
+export default Home
